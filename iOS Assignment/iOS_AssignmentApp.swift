9f2cf47a21cf70d981b187f52f4f8212960ebc1f
Ayush Singh <ayush.singh@webknot.in>
2023-08-29 15:06:49 +0530

    Initial Commit

---
+++ iOS Assignment/iOS_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_AssignmentApp.swift
+//  iOS Assignment
+//
+//  Created by Ayush Singh on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
